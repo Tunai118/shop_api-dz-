e16084532ff15e0ed61898da75d882c6d0e85a97
Tunai118 <btae1118@gmail.com>
2026-08-06 03:29:44 -0400

    Готово

--- product/views.py
+++ product/views.py
@@ -3,34 +3,34 @@ from .models import Category, Product, Review
 from .serializers import CategorySerializer, ProductReviewSerializer, ProductSerializer, ReviewSerializer
 from django.db.models import Avg, Count
 
-class CategoryListView(generics.ListAPIView):
+class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.annotate(products_count=Count("products"))
     serializer_class = CategorySerializer
 
 
-class CategoryDetailView(generics.RetrieveAPIView):
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class ProductReviewListView(generics.ListAPIView):
+class ProductReviewListView(generics.ListCreateAPIView):
     queryset = Product.objects.annotate(rating=Avg("reviews__stars"))
     serializer_class = ProductReviewSerializer
 
-class ProductListView(generics.ListAPIView):
+class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ProductDetailView(generics.RetrieveAPIView):
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ReviewListView(generics.ListAPIView):
+class ReviewListView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
-class ReviewDetailView(generics.RetrieveAPIView):
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
