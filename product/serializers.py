0b4582030d3f09287e17b45fe51610d9aba990c4
Tunai118 <btae1118@gmail.com>
2026-08-06 03:39:16 -0400

    Готово

--- product/serializers.py
+++ product/serializers.py
@@ -15,11 +15,26 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = "__all__"
 
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Минимум 3 символа")
+        return value
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Минимум 3 символа")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть выше 0")
+        return value
 
 
 class ProductReviewSerializer(serializers.ModelSerializer):
@@ -29,3 +44,13 @@ class ProductReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+
+    def validate_text(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Отзыв слишком короткий")
+        return value
+
+    def validate_stars(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Оценка должна быть от 1 до 5")
+        return value
